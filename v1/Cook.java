2cb5199f29705b7d44010fa821b66a1899c2ac6f
raywu6 <rwu6@stuy.edu>
2018-06-04 13:32:25 -0400

    add Cook class

---
+++ v1/Cook.java
@@ -0,0 +1,103 @@
+import java.util.ArrayList;
+
+public class Cook {
+
+    // inst vars
+    public String name;
+    private ArrayList<String> ingredientsOnHand;
+    private double salary;
+
+    // we chose to use an ArrayList
+    // b/c its size will be constantly changing...
+    
+    public Cook() {
+	name = "John Doe";
+	ingredientsOnHand = new ArrayList<String>();
+	salary = 0;
+    }
+
+    public Cook (String myName) {
+	this();          // call default constructor
+	name = myName;   // overwrite default name with specified name
+    }
+
+
+    public void setMoney (double amt) {
+	salary += amt;
+    }
+
+    
+    public double getMoney() {
+	return salary;
+    }
+
+
+    public int find(String str) {
+	String[] inv = Restaurant.inventory;
+	int indexLastElem = Restaurant.inventory.length - 1;
+	
+	return binarySearch(inv, str, 0, indexLastElem);
+    }
+
+
+    public int binarySearch(String[] arr, String target, int lo, int hi) {
+
+	while (left <= right) {
+	    int med = (lo + hi) / 2;
+
+	    // if found target
+	    if ( arr[med].compareTo(target) == 0 )
+		return med;
+
+	    // target < med, look at lower half of data
+	    else if ( arr[med].compareTo(target) > 0 )
+		hi = med - 1;
+
+	    // target > med, look at upper half of data
+	    else
+		lo = med + 1;
+	}
+
+	return -1;  // target not found
+    }
+    
+    
+    public void addIngredient(String str) {
+	if ( find(str) != -1 )
+	    // to do: if not already in hand
+	    ingredientsOnHand.add(str);
+	else
+	    System.out.println("This item is not in the restaurant's inventory!");
+    }
+
+
+    public String removeIngredient(String str) {
+
+	// takes care of erroneous duplicates
+	for (String s : ingredientsOnHand) {
+	    if ( s.equals(str) )
+		ingredientsOnHand.remove(s);
+	}
+    }
+
+    
+    public cook() {
+	// if every item in ingredientsOnHand matches w/ ones
+	// as defined by Customer's order
+    }
+
+    // for testing only
+    public static void main (String[] args) {
+	Cook a = new Cook();
+	System.out.println("name: " + a.name);
+	System.out.println("salary: " + a.salary);
+
+	Cook b = new Cook("Fred");
+	System.out.println("name: " + b.name);
+	System.out.println("salary: " + b.salary);
+
+	
+	
+    } // end main method
+	
+} // end class Cook
